a141a0119a776b685f80ca231773adf687082fe1
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-10-09 13:48:16 -0500

    wip

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -33,7 +33,16 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const {scheme_name} = req.body
+  if (
+    scheme_name === undefined ||
+    typeof scheme_name !== 'string' ||
+    !scheme_name.trim()
+  ) {
+    next({status: 400, message: "invalid scheme_name"})
+  } else {
+    next()
+  }
 }
 
 /*
